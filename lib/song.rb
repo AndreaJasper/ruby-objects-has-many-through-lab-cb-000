da5dbac224da4297f3f16de01a01216fea7a2fa3
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-12-19 18:53:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,16 @@
-class Song
+class Song 
+
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initizlize(name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@all << self
-    @genre.songs << self
   end
 
   def self.all
     @@all
   end
-end
+end 
