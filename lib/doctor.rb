7f9b20b6238e70e64dd4e1b7a8f44b1b603ae1ff
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-12-19 19:16:00 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,27 +1,28 @@
 class Doctor
-
-  attr_accessor :name
+  
+  attr_accessor :name, :patient, :appointment
+  
   @@all = []
+  
+ def initialize(name)
+   @name = name
+   @@all << self 
+ end
+ 
+ def self.all 
+   @@all 
+ end 
+ 
+ def new_appointment(patient,date)
+   Appointment.new(date, patient, self)
+ end
+ 
+ def appointments
+   Appointment.all.select{|aa| aa.doctor == self}
+ end
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
-  end
-
-  def patients
-    appointments.map {|appointment| appointment.patient}
-  end
-
-  def self.all
-    @@all
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, date, self)
-  end
-
-end 
+def patients
+  self.appointments.collect{|a| a.patient}
+end
+  
+end
