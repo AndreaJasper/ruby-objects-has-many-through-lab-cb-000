a7588524048af467c8072e43a3c623d13f9db08f
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-12-19 19:16:20 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,29 @@
-class Appointment
-
+class Appointment 
+  
   attr_accessor :date, :patient, :doctor
+  
   @@all = []
-
-  def initialize(patient, date, doctor)
-    @patient = patient
-    @date = date
-    @doctor = doctor
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-end 
+  
+ def initialize(date, patient, doctor)
+   @date = date
+   @patient = patient 
+   @doctor = doctor
+   
+   @@all << self 
+ end
+ 
+ 
+ def self.all 
+   @@all 
+ end 
+ 
+ def patients
+   self.patients
+ end
+ 
+ def doctors 
+   self.doctor 
+ end
+ 
+ 
+end
